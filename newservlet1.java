cd81a2e83ac061bb87072dec43cab057c8fd97bf
vision <126346231+vision-sahu@users.noreply.github.com>
2025-01-08 18:35:34 +0530

    Add files via upload

---
+++ newservlet1.java
@@ -0,0 +1,89 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+
+
+public class NewServlet1 extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String f=request.getParameter("first");
+        String l=request.getParameter("last");
+        String t=request.getParameter("email");
+        String p=request.getParameter("password");
+       
+        try (PrintWriter out = response.getWriter()) {
+           Class.forName("com.mysql.jdbc.Driver");
+           Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/company1","root",""); 
+           PreparedStatement ps=con.prepareStatement("INSERT INTO registration  VALUES (?,?,?,?)");
+           ps.setString(1,f);
+           ps.setString(2,l);
+           ps.setString(3,t);
+           ps.setString(4,p);
+           
+           
+           int x=ps.executeUpdate();
+           if(x>0){
+               out.println("successfull inserted");
+           }
+           else{
+               out.println("unsuccessfull");
+           }
+           
+        }
+        catch(Exception e){
+           System.out.println(e);
+        }
+        out.close();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
