466c73fe3b67f167bc1687448ab8bfef3efd7c76
vision <126346231+vision-sahu@users.noreply.github.com>
2025-01-08 18:34:07 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+document.getElementById('register').addEventListener('click', function() {
+    window.location.href = 'regn.html'; 
+});
+
+document.getElementById('forgot-pw').addEventListener('click', function() {
+    window.location.href = 'forgot-password.html';
+});
+
+document.getElementById('delete').addEventListener('click', function() {
+    window.location.href = 'delete.html';
+});
