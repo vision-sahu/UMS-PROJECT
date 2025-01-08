13edab34baf9e3832c21fc853802f46a6654abe8
vision <126346231+vision-sahu@users.noreply.github.com>
2025-01-08 18:31:12 +0530

    Add files via upload

---
+++ 2nd.js
@@ -0,0 +1,12 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+    window.location.href = 'login.html'; 
+});
+
+document.getElementById('aboutus').addEventListener('click', function() {
+    window.location.href = 'aboutus.html'; 
+});
+
+document.getElementById('contactus').addEventListener('click', function() {
+    window.location.href = 'contact.html'; 
+});
+
